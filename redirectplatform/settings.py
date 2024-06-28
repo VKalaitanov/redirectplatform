935234480ade8fbfb587bee0b476514840dc3f61
VKalaitanov <kalaytanov93@gmail.com>
2024-06-28 16:31:45 +0300

    add new settings postgres

--- redirectplatform/settings.py
+++ redirectplatform/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+import environ
+
+env = environ.Env()
+# environ.Env.read_env(env_file=Path('./docker/env/.env.prod'))
+environ.Env.read_env(env_file=Path('./.env.prod'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,17 +82,27 @@ WSGI_APPLICATION = 'redirectplatform.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'redirect_db',
+#         'USER': 'redirect_user',
+#         'PASSWORD': 'ASDKLA87SD6FG9875XCV',
+#         'HOST': 'postgres',
+#         'PORT': '5432'
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'redirect_db',
-        'USER': 'redirect_user',
-        'PASSWORD': 'ASDKLA87SD6FG9875XCV',
-        'HOST': 'postgres',
-        'PORT': '5432'
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('POSTGRES_HOST'),
+        'PORT': env('POSTGRES_PORT'),
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
